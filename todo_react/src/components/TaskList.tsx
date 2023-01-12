9b44fb8acf9053ce50fdbbbab475d661564dd189
Leonardo Gama <leogama09@gmail.com>
2023-01-12 18:20:56 -0300

    itens html

--- todo_react/src/components/TaskList.tsx
+++ todo_react/src/components/TaskList.tsx
@@ -16,6 +16,14 @@ const TaskList = ({taskList}: Props) => {
       {taskList.length > 0 ? (
         taskList.map((task) => (
           <div key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
             <p>{task.title}</p>
           </div>
         ))
