7df532c274b6ba341d106106a3b8e0b318b530f4
Leonardo Gama <leogama09@gmail.com>
2023-01-12 19:07:20 -0300

    Modal styles

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -26,7 +26,8 @@ function App() {
 
   return (
       <div>
-        <Modal />
+        <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}
+        />
         <Header />
         <main className={styles.main}>
         <div>
--- todo_react/src/components/Modal.tsx
+++ todo_react/src/components/Modal.tsx
@@ -3,14 +3,17 @@ import React from 'react'
 // CSS
 import styles from "./Modal.module.css"
 
-type Props = {}
+type Props = {
+    children: React.ReactNode
+}
 
-const Modal = (props: Props) => {
+const Modal = ({children}: Props) => {
   return (
     <div id="modal">
-        <div></div>
-        <div>
+        <div className={styles.fade}></div>
+        <div className={styles.modal}>
             <h2>Texto modal</h2>
+            {children}
         </div>
     </div>
   )
