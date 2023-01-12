f0ab5f39b297af8d4fa5dfaf706155ad955fc1cd
Leonardo Gama <leogama09@gmail.com>
2023-01-12 15:16:02 -0300

    Initial styles

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -4,11 +4,16 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+// CSS
+import styles from './App.module.css'
+
 function App() {
   return (
       <div>
         <Header />
+        <main className={styles.main}>
         <h1>Conteúdo...</h1>
+        </main>
         <Footer />
       </div>
   );
--- todo_react/src/components/Header.tsx
+++ todo_react/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import styles from './Header.module.css'
+
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
           <h1>React + TS Todo</h1>
     </header>
   )
