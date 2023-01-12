80909fc2c787585b5bdc228d601a0add25b4bf82
Leonardo Gama <leogama09@gmail.com>
2023-01-12 15:31:36 -0300

    Creating form

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -3,16 +3,26 @@ import React from 'react';
 // Components
 import Footer from './components/Footer';
 import Header from './components/Header';
+import TaskForm from './components/TaskForm';
 
 // CSS
 import styles from './App.module.css'
+import TaskList from './components/TaskList';
 
 function App() {
   return (
       <div>
         <Header />
         <main className={styles.main}>
-        <h1>Conteúdo...</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
         </main>
         <Footer />
       </div>
---
+++ todo_react/src/components/TaskList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const TaskList = (props: Props) => {
+  return (
+    <div>
+        <h1>Lista tarefas</h1>
+    </div>
+  )
+}
+
+export default TaskList
