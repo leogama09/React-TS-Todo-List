f4ba478c10cd238dfcc5f833279c746f15a3a4a0
Leonardo Gama <leogama09@gmail.com>
2023-01-12 19:20:36 -0300

    Closing modal event

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -24,6 +24,19 @@ function App() {
     )
   }
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if(display) {
+      modal!.classList.remove("hide")
+    } else {
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = (): void => {
+    hideOrShowModal(true)
+  }
+
   return (
       <div>
         <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}
@@ -41,7 +54,11 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask} />
+          <TaskList 
+            taskList={taskList} 
+            handleDelete={deleteTask} 
+            handleEdit={editTask}
+          />
         </div>
         </main>
         <Footer />
