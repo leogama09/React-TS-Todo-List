0e6417528b71adf7c63b38342877a76458a7450c
Leonardo Gama <leogama09@gmail.com>
2023-10-17 15:06:00 -0300

    login page

--- todo_react/src/pages/Login/Login.tsx
+++ todo_react/src/pages/Login/Login.tsx
@@ -1,10 +1,78 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+
+import { db } from '../../firebase/config'
+
+import { useAuthentication } from '../../hooks/useAuthentication'
 
 import styles from "./Login.module.css"
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const {createUser, error: authError, loading} = useAuthentication()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    setError("")
+
+    const user = {
+      email,
+      password
+    }
+
+    const res = await createUser(user)
+
+    console.log(res)
+  }
+
+  useEffect(() => {
+
+    if(authError) {
+      setError(authError)
+    }
+
+  }, [authError])
+
   return (
-    <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Sign In</h1>
+      <p>Log in to be able to use the system</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>E-mail:</span>
+          <input 
+            type="email" 
+            name="email" 
+            required 
+            placeholder="User E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input 
+            type="password" 
+            name="password" 
+            required 
+            placeholder="Insert your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Enter</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Loading...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
   )
 }
 
